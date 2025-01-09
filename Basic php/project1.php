427b194b5d09e322f6276e10b8ebfc9e8a01609c
dradya <adyatirvir29@gmail.com>
2025-01-10 00:05:13 +0530

    php

--- Basic php/project1.php
+++ Basic php/project1.php
@@ -1,5 +1,77 @@
-    <?php
-    $a = 4;
-    echo $a;
-    ?>
+<?php
+
+  $a=10;
+  $b=2;
+  echo"ARITHMETIC OPERATORS";
+  echo"\n";
+  $c=$a+$b;
+  echo $c;
+  echo"\n";
+  $d=$a-$b;
+  echo $d;
+  echo"\n";
+  $e=$a*$b;
+  echo $e;
+  echo"\n";
+  $f=$a/$b;
+  echo $f;
+  echo"\n";
+  
+  echo"RELATIONAL OPERATORS";
+  echo"\n";
+  $g=$a>$b;
+  echo $g;
+  echo"\n";
+  $h=$a<$b;
+  echo $h;
+  echo"\n";
+  $i=$a==$b;
+  echo $i;
+  echo"\n";
+  $j=$a!=$b;
+  echo $j;
+  echo"\n";
+  $k=$a<=$b;
+  echo $k;
+  echo"\n";
+  $l=$a>=$b;
+  echo $l;
+  echo"\n";
+  
+  echo"LOGICAL OPERATORS";
+  echo"\n";
+  $m=(($a>$b) && ($a<$b));
+  echo $m;
+  echo"\n";
+  $n=$a<$b || $a!=$b;
+  echo $n;
+  echo"\n";
+  $o=!($a<$b);
+  echo $o;
+  echo"\n";
+  
+  echo" Bitwise Operators";
+  echo"\n";
+  $p=$a&$b;
+  echo $p;
+  echo"\n";
+  $q=$a|$b;
+  echo $q;
+  echo"\n";
+  $r=$a^$b;
+  echo $r;
+  echo"\n";
+  $s=$a>>$b;
+  echo $s;
+  echo"\n";
+  $t=$a<<$b;
+  echo $t;
+  echo"\n";
+  $u=~$a;
+  echo $u;
+  echo"\n";
+  $v=~$b;
+  echo $v;
+  echo"\n";
+  ?>
    
