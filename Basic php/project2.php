ec6b2728eae51ac34190052cd9f6bc22dd3e28ec
dradya <adyatirvir29@gmail.com>
2025-01-10 12:40:51 +0530

    new

--- Basic php/project2.php
+++ Basic php/project2.php
@@ -4,22 +4,26 @@ $b=2;
 if($a%2==0 and $b%2==0)
 {
 $product=$a*$b;
+echo ("Both the numbers are even");
 echo $product;
 }
 else
     if($a%2!=0 and $b%2!=0)
     {
     $div=$a/$b;
+    echo ("a is odd, b is even");
     echo $div;
     }
 else
     if($a%2==0 and $b%2!=0)
     {
     $add=$a+$b;
+    echo ("a is even, b is odd");
     echo $add;
     }
 
 else
     $sub=$a-$b;
+    echo ("both the numbers are odd");
     echo $sub;
 ?>
